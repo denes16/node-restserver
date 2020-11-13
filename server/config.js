7eca6ad0b1c1907d76d05d796a417eef43627b9d
Denes <denesyt200@gmail.com>
2020-11-13 12:44:18 -0600

    Variable de entorno heroku

--- server/config.js
+++ server/config.js
@@ -9,6 +9,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de datos
 
-const urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://denes27:vDcseUnNTF6x8ByU@denes-cluster.bas3u.mongodb.net/cafe' ;
+const urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_DB_URL ;
 
 process.env.URL_DB = urlDB;
